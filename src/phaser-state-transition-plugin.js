7224f5f1d88e07522e2a91022a06600831003c8e
aaccurso <accurso.alan@gmail.com>
2014-12-13 00:26:48 -0300

    refactor: applies javascript best practices and improves comments

--- src/phaser-state-transition-plugin.js
+++ src/phaser-state-transition-plugin.js
@@ -22,8 +22,10 @@
   Phaser.Plugin.StateTransition.prototype.constructor = Phaser.Plugin.StateTransition;
 
   Phaser.Plugin.StateTransition.prototype.settings = function (options) {
+    var property;
+
     if (options) {
-      for (var property in options) {
+      for (property in options) {
         if (settings[property]) {
           settings[property] = options[property];
         }
@@ -37,46 +39,45 @@
     * Handles the state changes and transitions
     */
   Phaser.Plugin.StateTransition.prototype.to = function () {
-    var state = arguments[0];
+    var state = arguments[0],
+        _this = this,
+        _create;
 
-    /* Pause the game at first */
+    if (!state) {
+      throw 'No state passed.';
+    }
+    // In case last transition went wrong
+    this._destroy();
+    _create = this.game.state.states[state].create;
+
+    // Pause game to take world snapshot
     this.game.paused = true;
 
-    /* If there's a sprite there, destroy it */
-    if (this._cover) {
-      this._cover.destroy();
-    }
-
-    /* Create current state texture */
+    // Create current state texture
     this._texture = new Phaser.RenderTexture(this.game, this.game.width, this.game.height, 'cover');
 
-    /* Draw the current world to the render */
+    // Draw the current world to the render
     this._texture.renderXY(this.game.world, -this.game.camera.x, -this.game.camera.y);
 
-    /* If there's a state as a paramterer change the state and do the dew */
-    if (state) {
-      var _create = this.game.state.states[state].create, _this = this;
+    // Create current state cover sprite
+    this._cover = new Phaser.Sprite(this.game, 0, 0, this._texture);
+    this._cover.fixedToCamera = true;
+    this._cover.anchor.set(0.5);
 
-      this._cover = new Phaser.Sprite(this.game, 0, 0, this._texture);
-      this._cover.fixedToCamera = true;
-      this._cover.anchor.setTo(0.5,0.5);
+    // Instead of x/y we need to set the cameraOffset point
+    this._cover.cameraOffset.x = this.game.width / 2;
+    this._cover.cameraOffset.y = this.game.height / 2;
 
-      /* Instead of x/y we need to set the cameraOffset point */
-      this._cover.cameraOffset.x = this.game.width / 2;
-      this._cover.cameraOffset.y = this.game.height / 2;
+    // Extend state create method to add and animate cover
+    this.game.state.states[state].create = function() {
+      _create.call(_this.game.state.states[state]);
+      _this.game.add.existing(_this._cover);
+      _this._animateCover();
+    };
 
-      this.game.state.states[state].create = function() {
-        _create.call(_this.game.state.states[state]);
+    this.game.state.start.apply(this.game.state, arguments);
 
-        _this.game.add.existing(_this._cover);
-
-        _this._animateCover();
-      };
-
-      this.game.state.start.apply(this.game.state, arguments);
-    }
-
-    /* Resume the game */
+    // Resume the game
     this.game.paused = false;
   };
 
@@ -91,27 +92,27 @@
   };
 
   Phaser.Plugin.StateTransition.prototype._animateCover = function () {
-    if (settings && settings.properties) {
-      for (var property in settings.properties) {
-        if (typeof settings.properties[property] !== 'object') {
-          var _dummy = {};
-          _dummy[property] = settings.properties[property];
-          this._tween = this.game.add
-            .tween(this._cover)
-            .to(_dummy,
-              settings.duration,
-              settings.ease, true);
-        } else {
-          this._tween = this.game.add
-            .tween(this._cover[property])
-            .to(settings.properties[property],
-              settings.duration,
-              settings.ease, true);
-        }
-      }
+    var valuePoperty, property, tween;
 
-      this._tween.onComplete.addOnce(this._destroy, this);
+    for (property in settings.properties) {
+      if (typeof settings.properties[property] !== 'object') {
+        valuePoperty = {};
+        valuePoperty[property] = settings.properties[property];
+        tween = this.game.add
+          .tween(this._cover)
+          .to(valuePoperty,
+            settings.duration,
+            settings.ease, true);
+      } else {
+        tween = this.game.add
+          .tween(this._cover[property])
+          .to(settings.properties[property],
+            settings.duration,
+            settings.ease, true);
+      }
     }
+    // Since all tweens have the same duration we listen to the last one created
+    tween.onComplete.addOnce(this._destroy, this);
   };
 
   Phaser.Plugin.StateTransition.prototype._destroy = function () {
