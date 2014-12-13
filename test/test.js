eabdce79d0aa5792477d32aebfa9123290bc9979
aaccurso <accurso.alan@gmail.com>
2014-12-13 15:06:32 -0300

    test(init): tests phaser game initialization

--- test/test.js
+++ test/test.js
@@ -1,7 +1,42 @@
 'use strict';
 
 describe('test', function(){
-  it('Phaser should be defined', function() {
-    expect(Phaser).toBeDefined();
+  var canvas = document.createElement('canvas'),
+      game = new Phaser.Game(800, 600, Phaser.AUTO, canvas),
+      transition;
+
+  game.state.add('boot', function(game) {
+    this.game = game;
+    this.create = function() {
+      transition = game.plugins.add(Phaser.Plugin.StateTransition);
+      // this.game.state.start('StateA');
+    };
+  }, true);
+  game.state.add('StateA', function(game) {
+    this.game = game;
+    this.create = function() {
+      // TODO
+    };
+  });
+  game.state.add('StateB', function(game) {
+    this.game = game;
+    this.create = function() {
+      // TODO
+    };
+  });
+
+  beforeEach(function() {
+    game.state.start('boot');
+  });
+
+  it('Phaser.Plugin.StateTransition should be defined', function() {
+    expect(Phaser.Plugin.StateTransition).toBeDefined();
+  });
+
+  it('Transition to StateA', function() {
+    expect(Object.keys(game.state.states)).toEqual(['boot', 'StateA', 'StateB']);
+    expect(game.state.current).toBe('boot');
+    transition.to('StateA');
+    expect(game.state.current).toBe('StateA');
   });
 });
